98e453a989e040cf03a2c6e4cd7ebb0507840bdb
Felipe Ballesteros <felipeballesteros@msn.com>
2021-02-09 08:50:19 -0500

    Refactor everything to hooks, rearranged folder structure

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,51 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-import Temperature from "./Temperature/index.js";
+import Temperature from "../Temperature";
 
 const url =
   "https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=imperial&appid=f2d256778f4c877271b311c22dd7e42f";
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {},
-      loading: true,
-    };
-  }
-  getUrl = () => {
+const Main = () => {
+  const [data, setData] = useState({weatherInfo: {}, loading: true});
+
+  const getUrl = url => {
     axios
       .get(url)
       .then(res => {
-        const response = res.data;
-        this.setState({
-          data: response,
-          loading: false,
-        });
+        setData({weatherInfo: res.data, loading: false});
       })
       .catch(error => {
         throw error;
       });
   };
 
-  componentDidMount() {
-    this.getUrl();
+  useEffect(() => getUrl(url), []);
+
+  if (data.loading) {
+    return <div> LoAdInG ...</div>;
   }
 
-  render() {
-    const {loading, data} = this.state;
-
-    if (loading) {
-      return <div> LoAdInG ...</div>;
-    }
-
-    return (
-      <div>
-        <Temperature data={data.main} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Temperature data={data.weatherInfo.main} />
+    </div>
+  );
+};
 
 export default Main;
