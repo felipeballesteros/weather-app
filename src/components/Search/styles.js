b8979d67a30bc8f77883643dfc7137229235cce5
Felipe Ballesteros <felipeballesteros@msn.com>
2023-02-28 18:10:42 -0500

    fix small css issue, updated babelrc

--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -5,10 +5,8 @@ const SearchWrapper = styled.div`
   line-height: 0;
   display: flex;
   border-radius: 3px;
-  overflow: hidden;
   -webkit-box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.3);
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.3);
-
   opacity: 1;
   animation: fade 0.5s linear;
 
